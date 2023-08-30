f9aed9e61b88bcd905c45d4e21033cde9fec5e14
Susie Gordon <susieyoungchoi@gmail.com>
2023-08-30 09:28:02 -0400

    redo cutting grass with teeth function

--- game.py
+++ game.py
@@ -1,23 +1,28 @@
-# Start game
-user_input = input("Give any whole number to start the game")
-print(2 + int(user_input))
-
 # Store game data in a dictionary
 game_data = {
-    "point": 0,
+    "money": 0,
     "quit": False
 }
 
-while (True):
-    user_input = int(input("""
-                     Do you want to cut lawns with your teeth today?
-                     [1] Yes, make $1
-                     [2] No, quit the game  
-                    """))
-    if (user_input == 1):
-        game_data["point"] += 1
-        print(f"You now have ${game_data['point']}")
+# You are starting a landscaping business, but all you have are your teeth.
+# Using just your teeth, you can spend the day cutting lawns and make $1. You can do this as much as you want.
+def use_teeth(): 
+    while (True):
+        user_input = int(input("""
+                        Would you like to cut lawns with your teeth today?
+                        [1] Yes, earn $1
+                        [2] Not today
+                        """))
+                        
+        if (user_input == 1):
+            game_data["money"] += 1
+            print(f"You now have ${game_data['money']}")
 
-    if (user_input == 2):
-        game_data["quit"] = True
+        if (user_input == 2):
+            game_data["quit"] = True
+
+        if (game_data["quit"] == True):
+         print("You quit the game")
+         break
     
+use_teeth()
