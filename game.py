e60c20cad05be150d3313ed47b873ba58babd0e8
Susie Gordon <susieyoungchoi@gmail.com>
2023-08-30 10:35:59 -0400

    landscaper: user can use scissors to cut grass

--- game.py
+++ game.py
@@ -4,13 +4,30 @@ game_data = {
     "quit": False
 }
 
+# Using the rusty scissors, you can spend the day cutting lawns and make $5. You can do this as much as you want.
+def use_scissors():
+    while (True):
+        user_input = int(input(""" 
+                        Would you like to use the scissors to work and earn $5?
+                        [1] Yes! My teeth need a break!
+                        [2] No, I'll keep using my chompers 
+                        """))
+        if (user_input == 1):
+            game_data["money"] += 5
+            print(f"You now have ${game_data['money']}")
+
+
+        if (user_input == 2):
+            use_teeth()
+        
+
 # You are starting a landscaping business, but all you have are your teeth.
 # Using just your teeth, you can spend the day cutting lawns and make $1. You can do this as much as you want.
 def use_teeth(): 
     while (True):
         user_input = int(input("""
-                        Would you like to cut lawns with your teeth today?
-                        [1] Yes, earn $1
+                        Would you like to cut lawns with your teeth today and earn $1?
+                        [1] Yes!
                         [2] Not today
                         """))
                         
@@ -28,10 +45,10 @@ def use_teeth():
                 if (user_input == 1):
                     game_data["money"] -= 5
                     print(f"You now have ${game_data['money']}")
-
+                    use_scissors()
+         
                 if (user_input == 2):
                     continue
-
 
 
         if (user_input == 2):
